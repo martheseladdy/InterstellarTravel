07e47f4c1794c8b77683164d68fcb9848f5054a4
Marthese Ladvier <48213746+martheseladdy@users.noreply.github.com>
2023-07-29 20:44:51 +0100

    creation

---
+++ src/main/java/com/martheseladvier/interstellartravel/InterstellarTravelApplication.java
@@ -0,0 +1,13 @@
+package com.martheseladvier.interstellartravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterstellarTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterstellarTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martheseladvier/interstellartravel/InterstellarTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martheseladvier.interstellartravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterstellarTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
