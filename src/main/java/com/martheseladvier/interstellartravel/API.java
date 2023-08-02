3ed5cac15b1a132c8eedc2708857744a579984b0
Marthese Ladvier <48213746+martheseladdy@users.noreply.github.com>
2023-08-02 13:38:47 +0100

    API endpoints

--- src/main/java/com/martheseladvier/interstellartravel/API.java
+++ src/main/java/com/martheseladvier/interstellartravel/API.java
@@ -1,27 +1,81 @@
 package com.martheseladvier.interstellartravel;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import org.springdoc.core.*;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/home")
 public class API {
     //http://localhost:8080/swagger-ui/index.html
-  //  @Operation(
- //           summary = "Say hi, API!",
- //           description = "test API endpoint to ensure swagger/api documentation is correctly configured"
-//    )
- //   @ApiResponse(
- //           responseCode = "200",
- //           description = "Everything looks good to me, pal."
- //   )
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, world!";
+
+    @Operation(
+            summary = "Calculates the cheapest transport type for the specified journey.",
+            description = "Calculates the cost of a one-way transfer for a personal transport or UTS transport. Returns the cheapest transport method, and the calculated journey cost. Can optionally take passenger number, and parking days."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Your request is being processed."
+    )
+
+    @GetMapping("/trasport/{distance}")
+    public String getCheapestTransfer(
+        @PathVariable int distance,
+        @Parameter(description = "Number of passengers") @RequestParam(required = false) int passengers,
+        @Parameter(description = "Number of parking days") @RequestParam(required = false) int parking) {
+        //call method
+
+        return "Your costs here :)";
+        }
+
+    @Operation(
+            summary = "Fetch list of accelerators.",
+            description = "Returns information of all accelerators and their connections."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Your request is being processed."
+    )
+
+    @GetMapping("/accelerators")
+    public String getAccelerators(){
+        //call method
+
+        return "All accelerators";
     }
+
+    @Operation(
+            summary = "Fetch information for a specific accelerator.",
+            description = "Return connections of a specific accelerator."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Your request is being processed."
+    )
+
+    @GetMapping("/accelerators/{acceleratorID}")
+    public String getAcceleratorByID(){
+        //call method
+        return "Single accelerator information";
+    }
+
+    @Operation(
+            summary = "Calculate cheapest journey.",
+            description = "Evaluates shortest path between accelerator stations and returns the route."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Your request is being processed."
+    )
+    @GetMapping("/accelerators/{acceleratorID}/to/{targetAcceleratorID}")
+
+    public String getCheapestRoute(
+            @PathVariable int acceleratorID,
+            @PathVariable int targetAcceleratorID){
+        //call method
+
+        return "Your costs here :)";
+    }
+
 }
