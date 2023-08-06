3f85a6f9fa360cabb82e3175dac0797835171c9f
Marthese Ladvier <48213746+martheseladdy@users.noreply.github.com>
2023-08-06 15:04:21 +0100

    route tests

--- src/test/java/com/martheseladvier/interstellartravel/RouteTest.java
+++ src/test/java/com/martheseladvier/interstellartravel/RouteTest.java
@@ -8,13 +8,33 @@ class RouteTest {
     Route route = new Route();
     public List<Accelerator> testShortestRoute(){
 
+        List<Connection> connectionSol = new ArrayList<>(Arrays.asList(
+                new Connection("RAN", "Ran", 100),
+                new Connection("PRX", "Proxima", 90),
+                new Connection("SIR", "Sirius", 100),
+                new Connection("ARC", "Arrcturus", 200),
+                new Connection("ALD", "Aldermain", 250)
+        ));
+
+        Accelerator acceleratorSol = new Accelerator("SOL", "Sol", connectionSol);
+
+
+        List<Connection> connectionAls = new ArrayList<>(Arrays.asList(
+                new Connection("ALT", "Altair", 1),
+                new Connection("ALD", "Aldermain", 1)
+        ));
+
+        Accelerator acceleratorAls = new Accelerator("ALS", "Alshain", connectionAls);
+
+        route.shortestRoute(acceleratorSol, acceleratorAls);
+
+        List<String> expectedRoute = new ArrayList(Arrays.asList("SOL", "ARC", "DEN", "FOM", "ALS"));
+        assertEquals(expectedRoute,route.getRoute());
+        assertEquals(33.7,route.getCost());
+
+
         return null;
     }
-    public List<Accelerator> testGetRoute(){
-        return null;
-    }
-    public double testGetCost(){
-        return 0;
-    }
+
 
 }
