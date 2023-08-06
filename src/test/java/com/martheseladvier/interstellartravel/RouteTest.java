0e7ae065349519a5d353bbe567a879645f8668d9
Marthese Ladvier <48213746+martheseladdy@users.noreply.github.com>
2023-08-06 14:09:08 +0100

    debug

--- src/test/java/com/martheseladvier/interstellartravel/RouteTest.java
+++ src/test/java/com/martheseladvier/interstellartravel/RouteTest.java
@@ -1,32 +1,13 @@
 package com.martheseladvier.interstellartravel;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RouteTest {
     Route route = new Route();
-    public List<Accelerator> testShortestRoute(Accelerator from, Accelerator to){
-    //    List<Connection> connections1 = new ArrayList<>();
-     //   Connection conn1 = new Connection("ONE", "connection1", 10);
-    //    Connection conn2 = new Connection("TWO", "connection2", 20);
-     //   Connection conn3 = new Connection("TESTTO", "TestTo", 1);
-    //    connections1.add(conn1);
-    //    connections1.add(conn2);
-    //    connections1.add(conn3);
-    //    Accelerator fromAcc = new Accelerator("TESTFROM", "TestFrom", connections1);
+    public List<Accelerator> testShortestRoute(){
 
-     //   List<Connection> connections2 = new ArrayList<>();
-      //  Connection con1 = new Connection("ONE", "connection1", 8);
-     //   Connection con2 = new Connection("TWO", "connection2", 18);
-      //  Connection con3 = new Connection("TESTFROM", "TestFrom", 100);
-      //  connections1.add(con1);
-     //   connections1.add(con2);
-     //   connections1.add(con3);
-     //   Accelerator toAcc = new Accelerator("TESTTO", "TestTo", connections2);
-
-      //  route.shortestRoute(fromAcc, toAcc);
-
-        //implement after based on time restraints
         return null;
     }
     public List<Accelerator> testGetRoute(){
